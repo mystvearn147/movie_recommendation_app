d903eaea42ddfb65e82b41766f983df009557f9f
Ton <mystvearn147@gmail.com>
2022-07-14 17:04:55 +0700

    add pages flow

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:movie_recommendation_app/src/features/movie_flow/movie_flow.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
+
 import 'theme/custom_theme.dart';
+import 'features/movie_flow/movie_flow.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -75,7 +78,7 @@ class MyApp extends StatelessWidget {
                     return const SampleItemDetailsView();
                   case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const MovieFlow();
                 }
               },
             );
---
+++ lib/src/features/movie_flow/movie_flow.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class MovieFlow extends StatefulWidget {
+  const MovieFlow({Key? key}) : super(key: key);
+
+  @override
+  createState() => _MovieFlowState();
+}
+
+class _MovieFlowState extends State<MovieFlow> {
+  final pageController = PageController();
+
+  void nextPage() {
+    pageController.nextPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  void previousPage() {
+    pageController.previousPage(
+      duration: const Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+    );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: pageController,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Scaffold(
+          body: Container(
+            color: Colors.red,
+          ),
+        ),
+        Scaffold(
+          body: Container(
+            color: Colors.green,
+          ),
+        ),
+        Scaffold(
+          body: Container(
+            color: Colors.blue,
+          ),
+        ),
+        Scaffold(
+          body: Container(
+            color: Colors.yellow,
+          ),
+        ),
+      ],
+    );
+  }
+}
